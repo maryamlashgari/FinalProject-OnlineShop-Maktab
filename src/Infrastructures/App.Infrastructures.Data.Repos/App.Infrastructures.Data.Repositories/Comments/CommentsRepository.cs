a71a2944533f63199652d45e5adffff252b9f317
maryamLashgari <maryam.lashgarii@gmail.com>
2023-06-03 09:36:47 +0330

    add comment Repository

---
+++ src/Infrastructures/App.Infrastructures.Data.Repos/App.Infrastructures.Data.Repositories/Comments/CommentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using App.Domain.Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrastructures.Data.Repositories.Comments
+{
+    public class CommentsRepository: ICommentRepository
+    {
+
+    }
+}
