e6b038518a0f8d08bd42d1c636cd9418c410986e
maryamLashgari <maryam.lashgarii@gmail.com>
2023-08-04 10:53:55 +0330

    acount Controller and Register view

--- OnlineShop/Controllers/AccountController.cs
+++ OnlineShop/Controllers/AccountController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.ViewModels;
 
 namespace OnlineShop.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser<int>> _userManager;
+        private readonly SignInManager<IdentityUser<int>> _signInManager;
+
+        public AccountController(UserManager<IdentityUser<int>> userManager, SignInManager<IdentityUser<int>> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
         {
             return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser<int>
+                {
+                    UserName = vm.UserName,
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, vm.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect("~/");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, item.Description);
+                    }
+                }
+            }
+            return View(vm);
         }
     }
 }
